9c824c54260d45d8b3ed41880b7906ba413e55a1
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-25 11:03:11 -0300

    config global styles

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./routes"
 
+import "./sass/global.scss"
+
 createApp(App).use(router).mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>
+  <div class="contentWrapper">
     <h1>Hello World</h1>
   </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
+.contentWrapper {
+  max-width: 1200px;
+  height: 100vh;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 453px;
+  column-gap: 120px;
+  position: relative;
+}
 
+.contentSigned::before {
+  content: '';
+  height: 100vh;
+  width: 420px;
+  background: url('../assets/background.svg') no-repeat;
+  background-size: cover;
+  position: absolute;
+  right: -200px;
+  top: 0;
+}
 </style>
